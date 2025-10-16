c34a84e7a6c39b151f74807f7c7e28953d0ceae7
Ironhammer Std <2482911962@qq.com>
2025-10-16 11:58:47 +0800

    Implement Copy Protection Functions

---
+++ src/Launch_Impl.cpp
@@ -0,0 +1,189 @@
+﻿#include "Launch_Impl.h"
+#include "Windows.h"
+#include <VersionClass.h>
+
+
+namespace CopyProtection_Impl
+{
+
+    bool IsLauncherRunning()
+    {
+        if constexpr (UseVanillaImpl_Critical)
+        {
+            HANDLE hMutex;
+
+            Debug::LogString("COPYPROTECTION - Checking if launcher is running\n");
+
+            hMutex = CreateMutexA(0, 0, MutexGUID);
+            bool HasLauncher = (GetLastError() == ERROR_ALREADY_EXISTS);
+            if (hMutex)CloseHandle(hMutex);
+
+            Debug::Log("result was %d\n", HasLauncher);
+            return HasLauncher;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    bool NotifyLauncher()
+    {
+        if constexpr (UseVanillaImpl_Critical)
+        {
+            HANDLE hEvent;
+            DWORD NextCheckTime;
+            MSG Msg;
+
+            const DWORD MillisecondsOf1Minute = 60000;
+            const DWORD MillisecondsOf10Seconds = 10000;
+            
+            Debug::LogString("COPYPROTECTION - Notify launcher\n");
+
+            PeekMessageA(&Msg, 0, WM_USER, WM_USER, PM_NOREMOVE);
+            DWORD NotifyWaitEndTime = timeGetTime() + MillisecondsOf1Minute;
+
+            if (timeGetTime() < NotifyWaitEndTime)
+            {
+
+                while (1)
+                {
+                    hEvent = OpenEventA(EVENT_MODIFY_STATE, TRUE, NotifySignalGUID);
+                    if (hEvent)break;
+                    Sleep(0);
+                    if (timeGetTime() >= NotifyWaitEndTime)
+                    {
+                        Debug::LogString("***** Failed to notify launcher!\n");
+                        return false;
+                    }
+                }
+
+                SetEvent(hEvent);
+                CloseHandle(hEvent);
+                Debug::LogString("Launcher notified.\n");
+                Debug::LogString("Waiting for message from launcher.\n");
+                NextCheckTime = timeGetTime() + MillisecondsOf10Seconds;
+                if (timeGetTime() <= NextCheckTime)
+                {
+                    while (!PeekMessageA(
+                        &Msg, 0,
+                        Launcher_HereIAm_MessageID,
+                        Launcher_HereIAm_MessageID,
+                        PM_REMOVE) ||
+                        Msg.message != Launcher_HereIAm_MessageID)
+                    {
+                        Sleep(0);
+                        if (timeGetTime() > NextCheckTime)
+                        {
+                            Debug::LogString("***** Failed to notify launcher!\n");
+                            return false;
+                        }
+                    }
+                    _mProtectedData = (char*)MapViewOfFileEx((HANDLE)Msg.lParam, FILE_MAP_ALL_ACCESS, 0, 0, 0, 0);
+                    if (_mProtectedData)
+                        return true;
+                }
+            }
+            Debug::LogString("***** Failed to notify launcher!\n");
+            return false;
+		}
+        else
+        {
+            return true;
+        }
+    }
+
+    bool CheckVersion()
+    {
+        if constexpr (UseVanillaImpl_Critical)
+        {
+            LSTATUS RegQueryResult;
+            HKEY hKey;
+            DWORD RegKeycbData;
+            DWORD RegKeyType;
+            DWORD VersionFromRegister;
+
+            if (RegOpenKeyExA(HKEY_CLASSES_ROOT, Launcher_RegKey, 0, KEY_READ, &hKey))
+                return 0;
+            RegKeycbData = 4;
+            RegQueryResult = RegQueryValueExA(hKey, "ID", 0, &RegKeyType, (LPBYTE)&VersionFromRegister, &RegKeycbData);
+            RegCloseKey(hKey);
+            if (RegQueryResult || RegKeyType != REG_DWORD)
+                return 0;
+            return VersionFromRegister == VersionClass::Instance.Version_Number();
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    bool CheckProtectedData()
+    {
+        if constexpr (UseVanillaImpl_Critical)
+        {
+            const char* ProtectedStr;
+
+            Debug::LogString("COPYPROTECTION - Validating\n");
+
+            ProtectedStr = _mProtectedData;
+            if (!_mProtectedData)
+                ProtectedStr = mProtectData_Failed;
+            Debug::Log("_mProtectedData = %d (%s)\n", _mProtectedData, ProtectedStr);
+            if (_mProtectedData)
+                return strcmp(_mProtectedData, mProtectData_Success) == 0;
+
+            //Refolded Inline Function Calls
+            return CheckVersion();
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    void __fastcall DispatchLauncherMessage(MSG* pMsg)
+    {
+        if constexpr (UseVanillaImpl_Critical)
+        {
+            if (pMsg->message == Launcher_HereIAm_Message)
+            {
+                Debug::LogString("COPYPROTECTION - Received message from launcher.\n");
+                _mProtectedData = (char*)MapViewOfFileEx((HANDLE)pMsg->lParam, FILE_MAP_ALL_ACCESS, 0, 0, 0, 0);
+                if (_mProtectedData)
+                    Debug::Log("The message says: %s\n", _mProtectedData);
+                else
+                    Debug::LogString("***** MapViewOfFileEx() Failed!\n");
+            }
+        }
+        else return;
+    }
+
+    LSTATUS ShutDown()
+    {
+        if constexpr (UseVanillaImpl_Critical)
+        {
+            LSTATUS RegOpenResult;
+            HKEY phkResult;
+
+            Debug::LogString("COPYPROTECTION - Shutdown\n");
+            if (_mProtectedData)
+            {
+                UnmapViewOfFile(_mProtectedData);
+                _mProtectedData = nullptr;
+            }
+            RegOpenResult = RegOpenKeyExA(HKEY_CLASSES_ROOT, Launcher_RegKey, 0, KEY_ALL_ACCESS, &phkResult);
+            if (!RegOpenResult)
+            {
+                RegDeleteValueA(phkResult, "ID");
+                RegOpenResult = RegCloseKey(phkResult);
+            }
+            return RegOpenResult;
+        }
+        else
+        {
+            return ERROR_SUCCESS;
+        }
+    }
+}
+
---
+++ src/Launch_Impl.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+#include "ImplBase.h"
+#include "VanillaImpl.h"
+#include <Debug.h>
+
+namespace CopyProtection_Impl
+{
+	inline const char* MutexGUID = "48BC11BD-C4D7-466b-8A31-C6ABBAD47B3E";
+	inline const char* NotifySignalGUID = "D6E7FC97-64F9-4d28-B52C-754EDF721C6F";
+	inline const char* Launcher_RegKey = "WChat\\SysID";
+	inline const UINT Launcher_HereIAm_MessageID = 0xBEEFu;
+	inline const UINT Launcher_HereIAm_Message = 48879;
+
+	bool IsLauncherRunning();
+	FUNCTION_IMPLEMENT(0x49F5C0, IsLauncherRunning);
+
+	bool NotifyLauncher();
+	FUNCTION_IMPLEMENT(0x49F620, NotifyLauncher);
+
+	bool CheckVersion();
+	FUNCTION_IMPLEMENT(0x49F920, CheckVersion);
+
+	bool CheckProtectedData();
+	FUNCTION_IMPLEMENT(0x49F7A0, CheckProtectedData);
+
+	void __fastcall DispatchLauncherMessage(MSG* pMsg);
+	FUNCTION_IMPLEMENT(0x49F740, DispatchLauncherMessage);
+
+	LSTATUS ShutDown();
+	FUNCTION_IMPLEMENT(0x49F8B0, ShutDown);
+
+	inline const char* mProtectData_Failed = "EEK!";
+	inline const char* mProtectData_Success = "UIDATA,3DDATA,MAPS";
+	DEFINE_REFERENCE(char*, _mProtectedData, 0x89F75C)
+}
