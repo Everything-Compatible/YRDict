32eaf9d0dfe5c97154127b39dc0165faf12899d9
Ironhammer Std <2482911962@qq.com>
2025-10-17 23:27:51 +0800

    Update ChangeLog & examples

--- src/WinMain_Impl.cpp
+++ src/WinMain_Impl.cpp
@@ -2,6 +2,9 @@
 #include "Debug_Impl.h"
 #include <SystemData.h>
 #include <CRT.h>
+#include <Unsorted.h>
+#include <Debug.h>
+#include "Launch_Impl.h"
 
 namespace WinMain_Impl
 {
@@ -9,7 +12,7 @@ namespace WinMain_Impl
 	{
 		Debug_Impl::CloseLogFile("YRDict : Closing Log File at exit...\n");
 	}
-	
+
 
 	int __stdcall WinMain_Aux(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 	{
@@ -28,6 +31,81 @@ namespace WinMain_Impl
 
 	int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 	{
+		//Initialize system params
+		Game::hInstance = hInstance;
+		Game::MainThreadId = ::GetCurrentThreadId();
+
+		//Show that WW is genius
+		if constexpr (!UseVanillaImpl_Supplementary)
+		{
+			//WW is so genius that we cannot understand the idea behind these numbers
+			Debug::Log("%d-%d\n", 1660, 152);
+			Debug::Log("%d-%d\n", 1661, 76);
+			Debug::Log("%d-%d\n", 1662, 76);
+			Debug::Log("%d-%d\n", 1663, 19);
+			Debug::Log("%d-%d\n", 1664, 3608);
+			Debug::Log("%d-%d\n", 1665, 1752);
+			Debug::Log("%d-%d\n", 1666, 3600);
+			Debug::Log("%d-%d\n", 1667, 272);
+			Debug::Log("%d-%d\n", 1668, 456);
+			Debug::Log("%d-%d\n", 1669, 888);
+			Debug::Log("%d-%d\n", 1670, 120);
+			Debug::Log("%d-%d\n", 1671, 1824);
+			Debug::Log("%d-%d\n", 1672, 6040);
+			Debug::Log("%d-%d\n", 1673, 352);
+			Debug::Log("%d-%d\n", 1674, 760);
+			Debug::Log("%d-%d\n", 1675, 328);
+			Debug::Log("%d-%d\n", 1676, 116);
+			Debug::Log("%d-%d\n", 1677, 90296);
+			Debug::Log("%d-%d\n", 1678, 432);
+			Debug::Log("%d-%d\n", 1679, 1776);
+			Debug::Log("%d-%d\n", 1680, 3792);
+			Debug::Log("%d-%d\n", 1681, 24);
+			Debug::Log("%d-%d\n", 1682, 21868);
+			Debug::Log("%d-%d\n", 1683, 3608);
+			Debug::Log("%d-%d\n", 1684, 176);
+			Debug::Log("%d-%d\n", 1685, 700);
+			Debug::Log("%d-%d\n", 1686, 176);
+			Debug::Log("%d-%d\n", 1687, 676);
+			Debug::Log("%d-%d\n", 1688, 212);
+			Debug::Log("%d-%d\n", 1689, 160);
+			Debug::Log("%d-%d\n", 1690, 248);
+			Debug::Log("%d-%d\n", 1691, 224);
+			Debug::Log("%d-%d\n", 1692, 700);
+			Debug::Log("%d-%d\n", 1693, 72);
+			Debug::Log("%d-%d\n", 1694, 180);
+			Debug::Log("%d-%d\n", 1695, 164);
+			Debug::Log("%d-%d\n", 1696, 56);
+			Debug::Log("%d-%d\n", 1697, 2280);
+			Debug::Log("%d-%d\n", 1698, 3704);
+			Debug::Log("%d-%d\n", 1699, 14144);
+			Debug::Log("%d-%d\n", 1700, 256);
+			Debug::Log("%d-%d\n", 1701, 128);
+			Debug::Log("%d-%d\n", 1702, 432);
+			Debug::Log("%d-%d\n", 1703, 6336);
+			Debug::Log("%d-%d\n", 1704, 1024);
+			Debug::Log("%d-%d\n", 1705, 1);
+			Debug::Log("%d-%d\n", 1706, 1312);
+			Debug::Log("%d-%d\n", 1707, 240);
+			Debug::Log("%d-%d\n", 1708, 212);
+			Debug::Log("%d-%d\n", 1709, 8);
+			Debug::Log("%d-%d\n", 1710, 28);
+			Debug::Log("%d-%d\n", 1711, 4);
+			Debug::Log("%d-%d\n", 1712, 8);
+			Debug::Log("%d-%d\n", 1713, 16);
+			Debug::Log("%d-%d\n", 1714, 32);
+			Debug::Log("%d-%d\n", 1715, 24);
+		}
+
+		// return true if Launcher is found
+		if (!CopyProtection_Impl::IsLauncherRunning())
+		{
+			// CopyProtection -- Exit if Launcher isn't found
+			Debug::LogString("Launcher not running....Bail!\n");
+			return 0;
+		}
+
+
 		//To be Implemented
 	}
 }
